048e76accf3c5fc19e9f7c3a873e8a57c586d3d3
codehass <hassan.elouardy06@gmail.com>
2025-06-19 20:05:03 +0100

    create product service

---
+++ backend/src/main/java/com/hassan/backend/ProductService.java
@@ -0,0 +1,18 @@
+package com.hassan.backend;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts(){
+        return productRepository.findAll();
+    }
+}
