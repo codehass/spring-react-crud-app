e88c992fbd987db04a5602a424e5651b7a25590e
codehass <hassan.elouardy06@gmail.com>
2025-06-21 13:53:16 +0100

    setup routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./App.css";
+import ProductsListPage from "./pages/ProductsListPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import EditProductPage from "./pages/EditProductPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import AddProductPage from "./pages/AddProductPage";
 
 function App() {
-	return <div>Hello front end</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<ProductsListPage />} />
+				<Route path="/products" element={<ProductsListPage />} />
+				<Route path="/products/new" element={<AddProductPage />} />
+				<Route path="/products/:productId" element={<ProductDetailsPage />} />
+				<Route path="/products/:productId/edit" element={<EditProductPage />} />
+				<Route path="*" element={<NotFoundPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
