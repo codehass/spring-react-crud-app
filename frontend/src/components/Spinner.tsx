a4264721f17c6c8955e6aa63d8ec72a30e4c7392
codehass <hassan.elouardy06@gmail.com>
2025-06-22 16:19:14 +0100

    Create Spinner component

---
+++ frontend/src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+function Spinner() {
+	return (
+		<div className="animate-spin rounded-full h-16 w-16 border-t-8 border-violet-600 border-solid border-opacity-60"></div>
+	);
+}
+
+export default Spinner;
