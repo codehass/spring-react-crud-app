a18e75a5125e2efad8f00a942ef2a3d7be451eae
codehass <hassan.elouardy06@gmail.com>
2025-06-24 18:00:55 +0100

    update app routes adding homePage to home route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,22 +6,30 @@ import NotFoundPage from "./pages/NotFoundPage";
 import AddProductPage from "./pages/AddProductPage";
 import ProductDetails from "./components/ProductList";
 import AppLayout from "./components/AppLayout";
+import RequireAuth from "./components/RequireAuth";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<AppLayout />}>
-					<Route index element={<ProductDetails />} />
-					<Route path="products" element={<ProductDetails />} />
-					<Route path="products/new" element={<AddProductPage />} />
-					<Route path="products/:productId" element={<ProductDetailsPage />} />
-					<Route
-						path="products/:productId/edit"
-						element={<EditProductPage />}
-					/>
-				</Route>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/login" element={<LoginPage />} />
 
+				<Route
+					path="/products"
+					element={
+						<RequireAuth>
+							<AppLayout />
+						</RequireAuth>
+					}
+				>
+					<Route index element={<ProductDetails />} />
+					<Route path="new" element={<AddProductPage />} />
+					<Route path=":productId" element={<ProductDetailsPage />} />
+					<Route path=":productId/edit" element={<EditProductPage />} />
+				</Route>
 				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</BrowserRouter>
