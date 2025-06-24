212f13a674194cb26993afd5c9b6c7a9924b9dbb
codehass <hassan.elouardy06@gmail.com>
2025-06-24 18:49:50 +0100

    refactor product actions components

--- frontend/src/components/ProductActions.tsx
+++ frontend/src/components/ProductActions.tsx
@@ -1,40 +1,48 @@
-import { Link } from "react-router";
 import { Eye, Pencil, Trash2 } from "lucide-react";
+import { Link } from "react-router";
 
-function ProductActions({ id }: { id: number }) {
+function ProductActions({
+	id,
+	onDelete,
+}: {
+	id: number;
+	onDelete: (id: number) => void;
+}) {
 	return (
-		<div className="flex justify-end gap-2">
-			<Link
-				to={`/products/${id}`}
-				className="group p-2 rounded-md bg-gray-100 hover:bg-blue-100 transition"
-				title="View"
-			>
-				<Eye
-					size={18}
-					className="text-blue-600 group-hover:text-blue-800 transition"
-				/>
-			</Link>
-			<Link
-				to={`/products/${id}/edit`}
-				className="group p-2 rounded-md bg-gray-100 hover:bg-gray-200 transition"
-				title="Edit"
-			>
-				<Pencil
-					size={18}
-					className="text-gray-600 group-hover:text-gray-800 transition"
-				/>
-			</Link>
-			<button
-				// onClick={() => handleDelete(id)}
-				className="group p-2 rounded-md bg-gray-100 hover:bg-red-100 transition"
-				title="Delete"
-			>
-				<Trash2
-					size={18}
-					className="text-red-500 group-hover:text-red-700 transition"
-				/>
-			</button>
-		</div>
+		<>
+			<div className="flex justify-end gap-2">
+				<Link
+					to={`/products/${id}`}
+					className="group p-2 rounded-md bg-gray-100 hover:bg-blue-100 transition"
+					title="View"
+				>
+					<Eye
+						size={18}
+						className="text-blue-600 group-hover:text-blue-800 transition"
+					/>
+				</Link>
+				<Link
+					to={`/products/${id}/edit`}
+					className="group p-2 rounded-md bg-gray-100 hover:bg-gray-200 transition"
+					title="Edit"
+				>
+					<Pencil
+						size={18}
+						className="text-gray-600 group-hover:text-gray-800 transition"
+					/>
+				</Link>
+				<button
+					onClick={() => onDelete(id)}
+					className="group p-2 rounded-md bg-gray-100 hover:bg-red-100 transition"
+					title="Delete"
+				>
+					<Trash2
+						size={18}
+						className="text-red-500 group-hover:text-red-700 transition"
+					/>
+				</button>
+			</div>
+		</>
 	);
 }
 
